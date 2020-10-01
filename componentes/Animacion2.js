5f34111c2502769a7f7f93d383835539dfa834f9
FRH35865 <FRH35865@bancopatagonia.com.ar>
2020-09-30 23:29:37 -0300

    Agrego nuevo componente de animacion 2 inicial

---
+++ componentes/Animacion2.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Animacion2 = () => (
+    <View>
+        <Text>Animacion2</Text>
+    </View>
+);
+
+export default Animacion2;
