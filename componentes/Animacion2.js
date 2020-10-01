b190ccb76f62c5e61048f5179f90a75a7c1b45ce
FRH35865 <FRH35865@bancopatagonia.com.ar>
2020-09-30 23:37:21 -0300

    Completo la animacion 2 con una caja que se expande

--- componentes/Animacion2.js
+++ componentes/Animacion2.js
@@ -1,11 +1,40 @@
 
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Text, StyleSheet, Animated } from 'react-native';
 
-const Animacion2 = () => (
-    <View>
-        <Text>Animacion2</Text>
-    </View>
-);
+const Animacion2 = () => {
+
+    const [animacion] = useState(new Animated.Value(0)); 
+
+    useEffect(()=>{
+        Animated.timing(
+            animacion, {
+                toValue: 450,
+                duration: 1000
+            }
+        ).start();
+    }, []);
+
+    return(
+        <Animated.View
+            style={[
+                styles.caja,
+                {
+                    width: animacion,
+                    height: animacion
+                }
+            ]}
+        >
+            
+        </Animated.View>
+    )
+}
+
+const styles = StyleSheet.create({
+    caja: {
+        width: 100,
+        height: 100
+    }
+});
 
 export default Animacion2;
